9c8c9d8255309dcadb0a05d8a3a847cbafb795d2
eunduk2 <dmsqls6789@naver.com>
2024-07-22 17:07:59 +0900

    ViewBuilder

--- SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
@@ -9,15 +9,69 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello World")
-            .onAppear(perform: {
-                print("onAppear!")
-            })
-            .onDisappear(perform: {
-                print("onDisappear!")
-            })
+//        VStack {
+//            Text("Text 1")
+//                .foregroundColor(.red)
+//                .font(.largeTitle)
+//            Text("Text 2")
+//                .foregroundColor(.red)
+//                .font(.largeTitle)
+//            Text("Text 3")
+//                .foregroundColor(.green)
+//                .font(.largeTitle)
+//            Text("Text 4")
+//                .foregroundColor(.green)
+//                .font(.largeTitle)
+//        }
+        VStack {
+            MyVStack(color: .red) {
+                Text("Text 1")
+                Text("Text 2")
+            }
+            MyVStack(color: .green) {
+                Text("Text 3")
+                Text("Text 4")
+            }
+        }
     }
 }
+
+struct MyVStack<Content: View>: View {
+    let content: () -> Content
+    let color: Color
+    
+    init(color: Color, @ViewBuilder content: @escaping () -> Content) {
+        self.content = content
+        self.color = color
+    }
+    var body: some View {
+        VStack {
+            content()
+        }
+        .font(.largeTitle)
+        .foregroundColor(color)
+    }
+}
+
+//struct MyVStack<Content>: View {
+//    let content: () -> Content
+//    let color: Color
+//    let font: Font
+//    
+//    init(@ViewBuilder content: @escaping () -> Content, color: Color, font: Font) {
+//        self.content = content
+//        self.color = color
+//        self.font = font
+//    }
+//    
+//    var body: some View {
+//        VStack {
+//            content()
+//                .foregroundColor(color)
+//                .font(font)
+//        }
+//    }
+//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
