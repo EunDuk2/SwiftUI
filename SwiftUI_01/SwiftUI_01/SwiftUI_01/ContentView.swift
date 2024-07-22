b14e3bf8b8c470a1be003a74183a8ea16036e20b
eunduk2 <dmsqls6789@naver.com>
2024-07-22 15:33:46 +0900

    커스텀 수정자

--- SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
@@ -8,21 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    let hStack = HStack {
-        Text("Text 3")
-        Text("Text 4")
-    }
-    
     var body: some View {
-        VStack {
-            VStack {
-                Text("Text 1")
-                Text("Text 2")
-                hStack
-            }
-            Text("Text 5")
+        VStack{
+            Text("Text 1")
+                .modifier(StandardTitle())
+            Text("Text 2")
+                .modifier(StandardTitle())
         }
+    }
+}
+
+struct StandardTitle: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.largeTitle)
+            .background(Color.white)
+            .border(Color.gray, width: 2.0)
+            .shadow(color: Color.black, radius: 5, x: 0, y: 5)
     }
 }
 
