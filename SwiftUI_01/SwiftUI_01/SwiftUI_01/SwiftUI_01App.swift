0dbe9e90164847469bb02e61cbc9593848e2c005
eunduk2 <dmsqls6789@naver.com>
2024-07-22 10:25:09 +0900

    SwiftUI_01 파일 생성

---
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/SwiftUI_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_01App.swift
+//  SwiftUI_01
+//
+//  Created by Eunsung on 7/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
