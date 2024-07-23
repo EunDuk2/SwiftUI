93438f9d6763d1fd1882e5b2d1544c5ca2fc2561
eunduk2 <dmsqls6789@naver.com>
2024-07-23 11:07:15 +0900

    Spacer, alignment, padding

--- SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
@@ -9,69 +9,32 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-//        VStack {
-//            Text("Text 1")
-//                .foregroundColor(.red)
-//                .font(.largeTitle)
-//            Text("Text 2")
-//                .foregroundColor(.red)
-//                .font(.largeTitle)
-//            Text("Text 3")
-//                .foregroundColor(.green)
-//                .font(.largeTitle)
-//            Text("Text 4")
-//                .foregroundColor(.green)
-//                .font(.largeTitle)
-//        }
-        VStack {
-            MyVStack(color: .red) {
-                Text("Text 1")
-                Text("Text 2")
+        VStack(alignment: .center, spacing: 15) {
+            Text("Financial Results")
+                .font(.title)
+            
+            HStack {
+                Text("Q1 Sales")
+                    .font(.headline)
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("January")
+                    Text("February")
+                    Text("March")
+                }
+                Spacer()
+                VStack(alignment: .leading) {
+                    Text("$1000")
+                    Text("$200")
+                    Text("$3000")
+                }
+                .padding(5)
             }
-            MyVStack(color: .green) {
-                Text("Text 3")
-                Text("Text 4")
-            }
+            .padding(5)
         }
+        .padding(5)
     }
 }
-
-struct MyVStack<Content: View>: View {
-    let content: () -> Content
-    let color: Color
-    
-    init(color: Color, @ViewBuilder content: @escaping () -> Content) {
-        self.content = content
-        self.color = color
-    }
-    var body: some View {
-        VStack {
-            content()
-        }
-        .font(.largeTitle)
-        .foregroundColor(color)
-    }
-}
-
-//struct MyVStack<Content>: View {
-//    let content: () -> Content
-//    let color: Color
-//    let font: Font
-//    
-//    init(@ViewBuilder content: @escaping () -> Content, color: Color, font: Font) {
-//        self.content = content
-//        self.color = color
-//        self.font = font
-//    }
-//    
-//    var body: some View {
-//        VStack {
-//            content()
-//                .foregroundColor(color)
-//                .font(font)
-//        }
-//    }
-//}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
