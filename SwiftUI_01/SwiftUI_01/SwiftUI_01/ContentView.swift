a41fc76db20c78c62d2b59eb1efe5fa9a3e0d28c
eunduk2 <dmsqls6789@naver.com>
2024-07-23 11:46:49 +0900

    SwiftUI 프레임

--- SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
@@ -9,30 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .center, spacing: 15) {
-            Text("Financial Results")
-                .font(.title)
-            
-            HStack {
-                Text("Q1 Sales")
-                    .font(.headline)
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("January")
-                    Text("February")
-                    Text("March")
-                }
-                Spacer()
-                VStack(alignment: .leading) {
-                    Text("$1000")
-                    Text("$200")
-                    Text("$3000")
-                }
-                .padding(5)
-            }
-            .padding(5)
-        }
-        .padding(5)
+        Text("Hello World, How are you")
+            .font(.largeTitle)
+            .frame(minWidth: 50, maxWidth: 200, minHeight: 50, maxHeight: 200)
+            .edgesIgnoringSafeArea(.all)
     }
 }
 
