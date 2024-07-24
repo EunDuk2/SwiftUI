271e4330388c6ccde25528c7710f5373f5a068c9
eunduk2 <dmsqls6789@naver.com>
2024-07-24 14:13:07 +0900

    정적+동적 리스트 및 section 구현

--- SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/ContentView.swift
@@ -8,39 +8,45 @@
 import SwiftUI
 import Combine
 
+struct ToDoItem: Identifiable {
+    var id = UUID()
+    var task: String
+    var imageName: String
+}
+
+var listData: [ToDoItem] = [
+        ToDoItem(task: "Take out the trash", imageName: "trash.circle.fill"),
+        ToDoItem(task: "Pick up the kids", imageName: "person.2.fill"),
+        ToDoItem(task: "Wash the car", imageName: "car.fill")
+]
+
 struct ContentView: View {
     
-    @EnvironmentObject var timerData: TimerData
+    @State var toggleStatus = true
     
     var body: some View {
-        
-        NavigationView {
+        List {
             
-            VStack {
-                Text("Timer count = \(timerData.timeCount)")
-                    .font(.largeTitle)
-                    .fontWeight(.bold)
-                    .padding()
-                Button(action: resetCount, label: {
-                    Text("Reset Count")
+            Section(header: Text("Settings")) {
+                Toggle(isOn: $toggleStatus, label: {
+                    Text("Allow Notifications")
                 })
-                
-                NavigationLink(destination: SeconeView()) {
-                    Text("Next Screen")
-                }
-                .padding()
             }
+            Section(header: Text("To Do Tasks")) {
+                ForEach(listData) { item in
+                    HStack {
+                        Image(systemName: item.imageName)
+                        Text(item.task)
+                    }
+                }
+            }
+            
         }
-    }
-    
-    func resetCount() {
-        timerData.resetCount()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .environmentObject(TimerData())
     }
 }
--- SwiftUI_01/SwiftUI_01/SwiftUI_01/SwiftUI_01App.swift
+++ SwiftUI_01/SwiftUI_01/SwiftUI_01/SwiftUI_01App.swift
@@ -12,7 +12,6 @@ struct SwiftUI_01App: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
-                .environmentObject(TimerData())
         }
     }
 }
