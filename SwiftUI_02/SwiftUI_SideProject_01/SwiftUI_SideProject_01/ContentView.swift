ba2b8a2ae4524c4632b42ef60daf8cf53b122309
eunduk2 <dmsqls6789@naver.com>
2024-07-25 15:30:09 +0900

    Swift_02 FirstCommit

---
+++ SwiftUI_02/SwiftUI_SideProject_01/SwiftUI_SideProject_01/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SwiftUI_SideProject_01
+//
+//  Created by Eunsung on 7/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_02/SwiftUI_SideProject_01/SwiftUI_SideProject_01/SwiftUI_SideProject_01App.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SideProject_01App.swift
+//  SwiftUI_SideProject_01
+//
+//  Created by Eunsung on 7/25/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SideProject_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
