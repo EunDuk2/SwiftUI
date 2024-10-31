05cd785a50c077ce2e54af9882edf27fdff30b6a
eunduk2 <dmsqls6789@naver.com>
2024-10-31 23:07:34 +0900

    계산기 앱 1차 구현

---
+++ SwiftUI_Practice/SwiftUI_Practice/SwiftUI_Practice/Calculator.swift
@@ -0,0 +1,8 @@
+//
+//  Calculator.swift
+//  SwiftUI_Practice
+//
+//  Created by EUNSUNG on 10/31/24.
+//
+
+import Foundation
--- SwiftUI_Practice/SwiftUI_Practice/SwiftUI_Practice/ContentView.swift
+++ SwiftUI_Practice/SwiftUI_Practice/SwiftUI_Practice/ContentView.swift
@@ -8,14 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var num1: String = ""
+    @State var num2: String = ""
+    @State var result: Int = 0
+    
     var body: some View {
+        
+        // 레이블 -> 첫 번째 수: 텍스트필드 -> text field1
+        // 레이블 -> 두 번째 수: 텍스트필드 -> text field2
+        // 버튼 -> 덧셈 뺄셈 곱셈 나눗셈
+        // 레이블 -> 연산 결과: 100
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack { // alignment는 아래 위를 조정
+                Spacer()
+                Text("첫 번째 수: ")
+                // TextField의 레이블은 플레이스 홀더, text는 문자열만 가능
+                TextField("숫자를 입력하세요", text: $num1)
+            }
+            HStack {
+                Spacer()
+                Text("두 번째 수: ")
+                TextField("숫자를 입력하세요", text: $num2)
+            }
+            .padding(.bottom, 5)
+            HStack {
+                Spacer()
+                Button(action: {
+                    add()
+                }, label: {
+                    Text("덧셈")
+                })
+                Spacer()
+                Button(action: {
+                    minus()
+                }, label: {
+                    Text("뺄셈")
+                })
+                Spacer()
+                Button(action: {
+                    multiple()
+                }, label: {
+                    Text("곱셈")
+                })
+                Spacer()
+                Button(action: {
+                    divid()
+                }, label: {
+                    Text("나눗셈")
+                })
+                Spacer()
+            }
+            Text("결과: \(result)")
         }
         .padding()
+    }
+    
+    func add() {
+        result = Int(num1)! + Int(num2)!
+    }
+    func minus() {
+        result = Int(num1)! - Int(num2)!
+    }
+    func multiple() {
+        result = Int(num1)! * Int(num2)!
+    }
+    func divid() {
+        result = Int(num1)! / Int(num2)!
     }
 }
 
