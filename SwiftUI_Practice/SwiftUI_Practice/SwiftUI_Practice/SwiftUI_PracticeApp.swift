700798761766055a8204d9e63a42c57db0f094cd
eunduk2 <dmsqls6789@naver.com>
2024-10-31 21:46:36 +0900

    SwiftUI_Practice 프로젝트 생성

---
+++ SwiftUI_Practice/SwiftUI_Practice/SwiftUI_Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI_Practice
+//
+//  Created by EUNSUNG on 10/31/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
