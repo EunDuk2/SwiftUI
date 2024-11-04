c37e70077885dcb16e8a4797e7a233dc9079ba4e
eunduk2 <dmsqls6789@naver.com>
2024-11-05 00:52:51 +0900

    init

---
+++ Calculator_1/Calculator_1/Calculator_1/Calculator_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Calculator_1App.swift
+//  Calculator_1
+//
+//  Created by EUNSUNG on 11/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Calculator_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
