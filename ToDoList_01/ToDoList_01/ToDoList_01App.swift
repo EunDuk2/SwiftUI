51e974b8a98fcab2008352ca35191b009f05bfd8
eunduk2 <dmsqls6789@naver.com>
2024-11-12 21:43:56 +0900

    ToDoList_01 init

---
+++ ToDoList_01/ToDoList_01/ToDoList_01App.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoList_01App.swift
+//  ToDoList_01
+//
+//  Created by EUNSUNG on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoList_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
